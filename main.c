ed6057479c9d3f2998b4e1ac931680612955f814
Wejknis <weja077@gmail.com>
2018-11-22 18:05:12 +0100

    działa

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int a, b, c;
+
+    printf("Podaj a: "); scanf("%d", &a);
+    printf("Podaj b: "); scanf("%d", &b);
+
+    do {
+       if (a < b) { b -= a; } else { a -=b; }
+    } while (a != b);
+
+    printf("Wynik: %d, a");
+
     return 0;
 }
